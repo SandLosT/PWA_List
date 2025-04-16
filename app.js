9f2f35d6ed038e6a5569444a59d8db5f8facf714
Jhon <jhonrobertoalthaus@gmail.com>
2025-04-16 00:46:53 -0300

    Adicionando uma nova tela e novas funcinoalidades

--- app.js
+++ app.js
@@ -1,46 +1,7 @@
-const lista = document.getElementById('lista');
-const itemInput = document.getElementById('itemInput');
-
-let itens = JSON.parse(localStorage.getItem('itens')) || [];
-
-function renderizarLista() {
-  lista.innerHTML = '';
-  itens.forEach((item, index) => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-      <span>${item}</span>
-       <div class="botoes">
-        <button class="editar" onclick="editarItem(${index})">✏️</button>
-        <button class="excluir" onclick="removerItem(${index})">❌</button>
-      </div>
-    `;
-    lista.appendChild(li);
-  });
+function obterListas() {
+  return JSON.parse(localStorage.getItem('listas')) || [];
 }
 
-function adicionarItem() {
-  const valor = itemInput.value.trim();
-  if (valor) {
-    itens.push(valor);
-    localStorage.setItem('itens', JSON.stringify(itens));
-    itemInput.value = '';
-    renderizarLista();
-  }
+function salvarListas(listas) {
+  localStorage.setItem('listas', JSON.stringify(listas));
 }
-
-function editarItem(index) {
-  const novoValor = prompt('Editar item:', itens[index]);
-  if (novoValor) {
-    itens[index] = novoValor;
-    localStorage.setItem('itens', JSON.stringify(itens));
-    renderizarLista();
-  }
-}
-
-function removerItem(index) {
-  itens.splice(index, 1);
-  localStorage.setItem('itens', JSON.stringify(itens));
-  renderizarLista();
-}
-
-renderizarLista();
