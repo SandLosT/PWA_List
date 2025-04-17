239fec4a2744b058dafa40c16cce83e00658effc
douglaslima-pro <douglaslima-pro@outlook.com>
2025-04-17 19:19:50 -0300

    o projeto foi reestruturado

--- app.js
+++
@@ -1,7 +0,0 @@
-function obterListas() {
-  return JSON.parse(localStorage.getItem('listas')) || [];
-}
-
-function salvarListas(listas) {
-  localStorage.setItem('listas', JSON.stringify(listas));
-}
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,35 @@
-const CACHE_NAME = 'lista-cache-v1';
+const CACHE_NAME = 'make-it-list-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
+  '/images/favicon/android-chrome-192x192.png',
+  '/images/favicon/android-chrome-512x512.png',
+  '/images/favicon/apple-touch-icon.png',
+  '/images/favicon/favicon-16x16.png',
+  '/images/favicon/favicon-32x32.png',
+  '/images/favicon/favicon.ico',
+  '/pages/auth/login.html',
+  '/pages/auth/register.html',
+  '/pages/home/index.html',
+  '/pages/item/criar.html',
+  '/pages/item/editar.html',
+  '/pages/item/index.html',
+  '/pages/lista/criar.html',
+  '/pages/lista/editar.html',
+  '/pages/lista/index.html',
+  '/pages/lista/visualizar.html',
+  '/pages/usuario/editar.html',
+  '/pages/usuario/index.html',
+  '/styles/base/reset.css',
+  '/styles/base/typography.css',
+  '/styles/layout/aside.css',
+  '/styles/layout/footer.css',
+  '/styles/layout/header.css',
+  '/styles/layout/main.css',
+  '/styles/layout/sidebar.css',
+  '/styles/module/banner.css',
+  '/styles/theme/claro.css',
+  '/styles/theme/escuro.css',
   '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
