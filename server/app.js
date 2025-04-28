cbdf8de316ebee9f27566d862d393e56108ac9a6
Jhon <jhonrobertoalthaus@gmail.com>
2025-04-28 12:22:37 -0300

    Alterando modo de renderização para SSR

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
-var express = require('express')
-var app = express()
-var path = require('path')
+var express = require('express');
+var app = express();
+var path = require('path');
+const ejs = require('ejs');
+
 
 
 const listaRoutes = require('./routes/listaRoutes.js');
@@ -10,9 +12,10 @@ const usuarioRoutes = require('./routes/usuarioRoutes');
 // Middleware para JSON
 app.use(express.json());
 // static files
-const clientPath = path.join(__dirname, '..', 'public');
 
-app.use(express.json());
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, 'public/pages'))
+app.engine('html', ejs.renderFile)
 
 // 🌐 Rotas da API REST
 app.use('/api/listas', listaRoutes);
@@ -21,45 +24,42 @@ app.use('/api/usuarios', usuarioRoutes);
 // mapeamento das rotas
 // 🔗 Importação das rotas da API
 
-// Servir arquivos estáticos da pasta client
-app.use(express.static(clientPath));
-
 
 //home
   app.get('/', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'home', 'index.html'));
+    res.render(path.join(clientPath, 'pages', 'home', 'index.html'));
   });
 //crud de itens
   app.get('/item', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'item', 'index.html'));
+    res.render(path.join(clientPath, 'pages', 'item', 'index.html'));
   });
   app.get('/item/criar', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'item', 'criar.html'));
+    res.render(path.join(clientPath, 'pages', 'item', 'criar.html'));
   });
   app.get('/item/editar', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'item', 'editar.html'));
+    res.render(path.join(clientPath, 'pages', 'item', 'editar.html'));
   });
   
 //crud de listas
   app.get('/lista', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'lista', 'index.html'));
+    res.render(path.join(clientPath, 'pages', 'lista', 'index.html'));
   });
   app.get('/lista/criar', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'lista', 'criar.html'));
+    res.render(path.join(clientPath, 'pages', 'lista', 'criar.html'));
   });
   app.get('/lista/editar', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'lista', 'editar.html'));
+    res.render(path.join(clientPath, 'pages', 'lista', 'editar.html'));
   });
   app.get('/lista/visualizar', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'lista', 'visualizar.html'));
+    res.render(path.join(clientPath, 'pages', 'lista', 'visualizar.html'));
   });
 
 //crud de usuarios
   app.get('/usuario', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'usuario', 'index.html'));
+    res.render(path.join(clientPath, 'pages', 'usuario', 'index.html'));
   });
   app.get('/usuario/editar', (req, res) => {
-    res.sendFile(path.join(clientPath, 'pages', 'usuario', 'editar.html'));
+    res.render(path.join(clientPath, 'pages', 'usuario', 'editar.html'));
   });
 
 module.exports =  app;
