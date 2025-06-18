cdc7aaeda83ea543c6866ac52a1973f88834acc1
Jhon <jhonrobertoalthaus@gmail.com>
2025-06-18 10:26:46 -0300

    Implementação das variáveis de ambinte

--- api/firebase.js
+++ api/firebase.js
@@ -1,9 +1,25 @@
+const dotenv = require('dotenv');
 const admin = require('firebase-admin');
-const serviceAccount = require('./chave-firebase.json');
+const zlib = require('zlib');
+
+dotenv.config({ path: '../.env.firebase' });
+
+if (!process.env.CHAVE_FIREBASE_KEY) {
+  throw new Error('Variável CHAVE_FIREBASE_KEY não encontrada no .env.firebase');
+} else {
+  console.log('Variável CHAVE_FIREBASE_KEY carregada com sucesso.');
+}
+
+const chaveBase64SemQuebra = process.env.CHAVE_FIREBASE_KEY.replace(/(\r\n|\n|\r|\s)/gm, '');
+
+const bufferCompactado = Buffer.from(chaveBase64SemQuebra, 'base64');
+const jsonString = zlib.gunzipSync(bufferCompactado).toString('utf-8');
+const serviceAccount = JSON.parse(jsonString);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
+
 module.exports = { db };
